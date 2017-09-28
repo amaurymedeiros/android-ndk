7f86cc88c37a22347b26701d14ffdcdfa6c839c8
Amaury Medeiros <amaurymedeiros@gmail.com>
2017-09-28 19:29:30 +0100

    hello-jni: cleanup code irrelevant for testing
    We're only interested in triggering methods that run for a predictable
    amount of time, therefore we can remove other methods and UI components.

--- hello-jni/app/src/main/java/com/example/hellojni/HelloJni.java
+++ hello-jni/app/src/main/java/com/example/hellojni/HelloJni.java
@@ -26,14 +26,9 @@ public class HelloJni extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /* Retrieve our TextView and set its content.
-         * the text is retrieved by calling a native
-         * function.
-         */
         setContentView(R.layout.activity_hello_jni);
-        TextView tv = (TextView)findViewById(R.id.hello_textview);
-        tv.setText( stringFromJNI() );
 
+        // Sleep for 10 seconds, then sleep for 5 more.
         Button bt1 = (Button)findViewById(R.id.button1);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +38,7 @@ public class HelloJni extends AppCompatActivity {
             }
         });
 
+        // Sleep for 4 seconds.
         Button bt2 = (Button)findViewById(R.id.button2);
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,29 +47,12 @@ public class HelloJni extends AppCompatActivity {
             }
         });
     }
-    /* A native method that is implemented by the
-     * 'hello-jni' native library, which is packaged
-     * with this application.
-     */
-    public native String  stringFromJNI();
 
     public native void  sleep10();
 
     public native void  sleep5();
 
     public native void  sleep4();
-
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-    public native String  unimplementedStringFromJNI();
 
     /* this is used to load the 'hello-jni' library on application
      * startup. The library has already been unpacked into
